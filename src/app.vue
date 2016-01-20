aa64745f01ee524259ea5b05ae27bf50922d8748
trump27 <smilebowl-trump27@yahoo.co.jp>
2016-01-20 20:49:44 +0900

    add navbar.vue

--- src/app.vue
+++ src/app.vue
@@ -1,5 +1,4 @@
 <style lang="stylus">
-// .view
 .route-transition
   transition all .5s ease
 .route-leave
@@ -14,30 +13,7 @@
 
 <template>
   <div>
-    <nav class="navbar navbar-default">
-      <div class="container-fluid">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navmenu">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#">Board4</a>
-        </div>
-
-        <div class="collapse navbar-collapse" id="navmenu">
-          <ul class="nav navbar-nav">
-            <li><a v-link="'/todo'">Todo</a></li>
-            <li><a v-link="'/memo'">Memo</a></li>
-            <li><a v-link="{ path: '/home' }">Home</a></li>
-          </ul>
-          <ul class="nav navbar-nav navbar-right">
-            <li><a v-if="authenticated" @click="signout" href="#">Sign out</a></li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+    <navbar></navbar>
     <div class="container">
       <router-view transition="route"></router-view>
     </div>
@@ -46,12 +22,17 @@
 
 <script>
 import { auth } from './auth/auth.js'
+import Navbar from './components/navbar.vue'
 
 export default {
   data () {
     return {
       authenticated: false
     }
+  },
+
+  components: {
+    navbar: Navbar
   },
 
   created: function () {
@@ -62,17 +43,12 @@ export default {
   events: {
     'signinNotify': function () {
       this.authenticated = true
-    }
-  },
-
-  methods: {
-
-    signout () {
+    },
+    'signoutNotify': function () {
       this.authenticated = false
-      auth.signout()
       this.$route.router.go('/')
     }
-
   }
+
 }
 </script>
---
+++ src/components/navbar.vue
@@ -0,0 +1,39 @@
+<template>
+  <nav class="navbar navbar-default">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navmenu">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="#">Board4</a>
+      </div>
+      <div class="collapse navbar-collapse" id="navmenu">
+        <ul class="nav navbar-nav">
+          <li><a v-link="'/todo'">Todo</a></li>
+          <li><a v-link="'/memo'">Memo</a></li>
+          <li><a v-link="{ path: '/home' }">Home</a></li>
+        </ul>
+        <ul class="nav navbar-nav navbar-right">
+          <li><a v-if="$root.authenticated" @click="signout" href="#">Sign out</a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+import { auth } from '../auth/auth.js'
+
+export default {
+
+  methods: {
+    signout () {
+      auth.signout()
+      this.$dispatch('signoutNotify')
+    }
+  }
+}
+</script>
